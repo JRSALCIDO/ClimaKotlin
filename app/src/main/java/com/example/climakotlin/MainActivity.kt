8abca049743324e52bd04f79ff4b253520b013da
JRSALCIDO <salcido.12311@gmail.com>
2023-05-17 17:36:45 -0700

    Commit total

---
+++ app/src/main/java/com/example/climakotlin/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.climakotlin
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setTitle(R.string.app_name)
+    }
+}
